7034977654f9c93a9bf47227f1f71cc6b6327569
kitzune-no-aki <github2@go-nihon.com>
2022-02-23 13:12:05 +0100

    backgroundColor changes with promises

--- app.js
+++ app.js
@@ -21,3 +21,24 @@ function colorize() {
     this.style.backgroundColor = makeRandColor();
     this.style.color = makeRandColor();
 }
+
+const delayedColorChange = (color, delay) =>{
+    return new Promise((resolve, reject) =>{
+        setTimeout(() =>{
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay)
+    })
+}
+
+delayedColorChange('#fbf8cc', 3000)
+    .then(() =>delayedColorChange('#fde4cf',3000))
+    .then(() =>delayedColorChange('#ffcfd2',3000))
+    .then(() =>delayedColorChange('#f1c0e8',3000))
+    .then(() =>delayedColorChange('#cfbaf0',3000))
+    .then(() =>delayedColorChange('#a3c4f3',3000))
+    .then(() =>delayedColorChange('#90dbf4',3000))
+    .then(() =>delayedColorChange('#8eecf5',3000))
+    .then(() =>delayedColorChange('#98f5e1',3000))
+    .then(() =>delayedColorChange('#b9fbc0',3000))
+    .then(() =>delayedColorChange('lightslategrey',3000))
